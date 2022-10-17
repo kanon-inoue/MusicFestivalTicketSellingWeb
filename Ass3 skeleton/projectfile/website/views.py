670ee5942ae3223d22f68930289026a6e9f8584d
Kanon Inoue <n11186267@qut.edu.au>
2022-10-17 14:48:19 +1000

    yes

--- Ass3 skeleton/projectfile/website/views.py
+++ Ass3 skeleton/projectfile/website/views.py
@@ -5,4 +5,5 @@ bp = Blueprint('main', __name__)
 
 @bp.route('/')
 def index():
-    return '<h1>Starter code for the assessment<h1>'
+    return render_template('index.html')
+    ##events = events)

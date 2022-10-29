4a8f8383376a7623051dea9e7d2a83ea4302fdc4
JinerQi777 <n11342099@qut.edu.au>
2022-10-29 14:01:13 +1000

    register/login form

--- Ass3 skeleton/projectfile/website/forms.py
+++ Ass3 skeleton/projectfile/website/forms.py
@@ -1,23 +1,26 @@
-
 from flask_wtf import FlaskForm
-from wtforms.fields import TextAreaField,SubmitField, StringField, PasswordField
+from wtforms.fields import TextAreaField, SubmitField, StringField, PasswordField
 from wtforms.validators import InputRequired, Length, Email, EqualTo
-#creates the login information
+from flask_wtf.file import FileRequired, FileField, FileAllowed
+
+
+    
+#User login
 class LoginForm(FlaskForm):
     user_name=StringField("User Name", validators=[InputRequired('Enter user name')])
     password=PasswordField("Password", validators=[InputRequired('Enter user password')])
     submit = SubmitField("Login")
 
- # this is the registration form
+#User register
 class RegisterForm(FlaskForm):
     user_name=StringField("User Name", validators=[InputRequired()])
     email_id = StringField("Email Address", validators=[Email("Please enter a valid email")])
+    
     #linking two fields - password should be equal to data entered in confirm
     password=PasswordField("Password", validators=[InputRequired(),
                   EqualTo('confirm', message="Passwords should match")])
     confirm = PasswordField("Confirm Password")
-
-
-
     #submit button
     submit = SubmitField("Register")
+
+

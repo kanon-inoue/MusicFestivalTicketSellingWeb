af2fc67c721d368786dd2457f94275c21f0fafae
FocusedLoop <joshuawlod2003@gmail.com>
2022-11-01 20:53:25 +1000

    models changes

--- Ass3 skeleton/projectfile/website/models.py
+++ Ass3 skeleton/projectfile/website/models.py
@@ -119,3 +119,68 @@ class Booking(db.Model):
 
     def __repr__(self):
         return "Booking ID: {0}, # Tickets: {1} Booking Owner: {2} Event ID:{3}".format(self.id, self.tickets_booked, self.user_id, self.event_id)
+
+
+# OLD CODE (Problem use the newer code more as its more relevant to our assignment (only really have to remove a few lines and make some edits for it to work with our code))
+
+from . import db
+from datetime import datetime
+from flask_login import UserMixin
+#test
+class User(db.Model, UserMixin):
+    __tablename__='users' 
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), index=True, unique=True, nullable=False)
+    emailid = db.Column(db.String(100), index=True, nullable=False)
+	# the storage should be at least 255 chars long
+    password_hash = db.Column(db.String(255), nullable=False)
+
+    # relation to call user.comments and comment.created_by
+    comments = db.relationship('Comment', backref='user')
+
+
+
+class Tickets(db.Model): # Rename to booking
+    __tablename__ = 'Tickets'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.column(db.Integer, foreign_key=True) # use db.ForeignKey instead look at how its done in comments
+    user_name = db.Column(db.String(80)) # Shouldnt need as the user_id should link it to the user table
+    description = db.Column(db.String(200)) # Don't need either
+    currency = db.Column(db.String(3)) # Dont need
+    # ... Create the Comments db.relationship
+	# relation to call destination.comments and comment.destination
+    comments = db.relationship('Comment', backref='destination') #Remove tickets dont need comments
+    # Need to add event id to link it to the event!
+    
+	
+    def __repr__(self): #string print method
+        return "<Name: {}>".format(self.name)
+
+class Comments(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(400))
+    created_at = db.Column(db.DateTime, default=datetime.now())
+    #add the foreign keys
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    events_id = db.Column(db.Integer, db.ForeignKey('destinations.id'))
+
+
+    def __repr__(self):
+        return "<Comment: {}>".format(self.text)
+
+
+
+class Events(db.Model):
+    __tablename__ = 'events'
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(400))
+    image = db.Column(db.String(400))
+    created_at = db.Column(db.DateTime, default=datetime.now())
+    #add the foreign keys
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    comments_id = db.Column(db.Integer, db.ForeignKey('destinations.id'))
+    # add currency or price
+    # Need to add date and place
+
+# Models will need some redoing. In theory this shouldnt take that long, I am also happy to help do models

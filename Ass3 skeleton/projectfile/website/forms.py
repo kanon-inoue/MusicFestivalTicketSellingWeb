27c9769c69b25833860642a3763e288143efe4b0
Kanon Inoue <109997852+11186267@users.noreply.github.com>
2022-11-01 19:44:22 +1000

    yes

--- Ass3 skeleton/projectfile/website/forms.py
+++ Ass3 skeleton/projectfile/website/forms.py
@@ -1,31 +1,3 @@
-from flask_wtf import FlaskForm
-from wtforms.fields import TextAreaField, SubmitField, StringField, PasswordField
-from wtforms.validators import InputRequired, Length, Email, EqualTo
-from flask_wtf.file import FileRequired, FileField, FileAllowed
-
-
-    
-#User login
-class LoginForm(FlaskForm):
-    user_name=StringField("User Name", validators=[InputRequired('Enter user name')])
-    password=PasswordField("Password", validators=[InputRequired('Enter user password')])
-    submit = SubmitField("Login")
-
-#User register
-class RegisterForm(FlaskForm):
-    user_name=StringField("User Name", validators=[InputRequired()])
-    email_id = StringField("Email Address", validators=[Email("Please enter a valid email")])
-    
-    #linking two fields - password should be equal to data entered in confirm
-    password=PasswordField("Password", validators=[InputRequired(),
-                  EqualTo('confirm', message="Passwords should match")])
-    confirm = PasswordField("Confirm Password")
-    #submit button
-    submit = SubmitField("Register")
-
-
-
-"""
 import re
 from website.models import MusicGenre, EventState, EventStatus
 from flask_wtf import FlaskForm
@@ -137,5 +109,3 @@ class BookingForm(FlaskForm):
     tickets_required = IntegerField(
         'How many tickets would you like to book?', default='1', validators=[InputRequired()])
     submit = SubmitField('Confirm Booking')
-
-"""
